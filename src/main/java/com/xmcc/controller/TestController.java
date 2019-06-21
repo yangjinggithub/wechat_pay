160f400c21ce2b4d4326f7fb8d7d57262e14a422
yangjinggithub <1421060733@qq.com>
2019-06-21 11:04:06 +0800

    第二次提交

--- src/main/java/com/xmcc/controller/TestController.java
+++ src/main/java/com/xmcc/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class TestController {
 
-    @RequestMapping("/world")
+    @RequestMapping("/hello")
     public String hello(){
         return "hello spring-boot";
     }
